45d66c418c4c2d928745b7bfae3d34d5e487a2d6
sangil312 <sangil0307@naver.com>
2026-01-15 13:37:44 +0900

    refactor: Rest Docs 설정 및 테스트 태스크 추가

--- core/core-api/src/test/java/com/dev/core/ecommerce/IntegrationTestSupport.java
+++ core/core-api/src/test/java/com/dev/core/ecommerce/IntegrationTestSupport.java
@@ -1,8 +1,10 @@
 package com.dev.core.ecommerce;
 
+import org.junit.jupiter.api.Tag;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
+@Tag("integration")
 @ActiveProfiles("local-test")
 @SpringBootTest(classes = CoreApiApplication.class)
 public abstract class IntegrationTestSupport {
